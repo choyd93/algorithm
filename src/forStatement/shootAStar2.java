a23f1da0a70d15c4c67dbff958344dc6ec9e0e7c
youngdong <choyd93@naver.com>
2021-06-21 20:21:48 +0900

    백준 별찍기-1 완

--- src/forStatement/shootAStar2.java
+++ src/forStatement/shootAStar2.java
@@ -1,6 +1,6 @@
 package forStatement;
 import java.util.Scanner;
-public class shootAStar1 {
+public class shootAStar2 {
 
 	public static void main(String[] args) {
 		Scanner my = new Scanner(System.in);
@@ -9,7 +9,7 @@ public class shootAStar1 {
 		
 		for(int i = 1; i<=a; i++) {
 			for(int j = 1; j<=a; j++) {
-				System.out.print("*");
+				System.out.format("%6d", "*");
 				if (j>=i)
 					break; } 
 			System.out.println();
