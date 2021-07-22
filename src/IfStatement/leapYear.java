f6c39d182997c675b54efccfd9aa7b9d6bd59ebf
youngdong <choyd93@naver.com>
2021-07-23 08:55:46 +0900

    baekjoon if Statement leapYear complete

---
+++ src/IfStatement/leapYear.java
@@ -0,0 +1,23 @@
+package IfStatement;
+
+import java.util.Scanner;
+
+public class leapYear {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int year = scanner.nextInt();
+        
+        while(!(year >= 1 & year <= 4000)) {
+            year = scanner.nextInt();
+        }
+        
+        if(year%4 == 0 && year%100 != 0 || year%400 == 0) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+
+    }
+}
